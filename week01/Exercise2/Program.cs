a83036c94a5691b80156f6eb9712cd12cb6fdc22
maalcorn <meowcorn@icloud.com>
2025-05-09 23:30:03 -0700

    inserted code as instructed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("This is in C#.");
+        Console.Write("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
+        Console.Write("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your full name is {firstName} {lastName}.");
     }
 }
