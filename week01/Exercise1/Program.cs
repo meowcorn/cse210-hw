8c66112d9cd4814f1843b81557b5e84cb6502b99
maalcorn <meowcorn@icloud.com>
2025-05-09 23:15:42 -0700

    Update greeting message and add color input functionality

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 1.");
+        Console.WriteLine("This is in C#.");
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}.");
+    
     }
 }
