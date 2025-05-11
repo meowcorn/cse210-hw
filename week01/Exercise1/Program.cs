58d79553c81f3c62ae0b57b2c842f88533fc1216
maalcorn <meowcorn@icloud.com>
2025-05-11 16:25:37 -0700

    Exercise 1 does not run (build?) error message says file is locked and my attempts to unlock file have failed. I added Exercise 1 first, last, full name coding to Exercise 2 file.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("This is in C#.");
         Console.Write("What is your favorite color?");
         string color = Console.ReadLine();
@@ -14,29 +14,7 @@ class Program
         Console.Write("What is your last name?");
         string lastName = Console.ReadLine();
         Console.WriteLine($"Your full name is {firstName} {lastName}.");
-        Console.Write("What is your grade percentage?");
-        string answer = Console.ReadLine();
-        int percent = int.Parse(answer);
-        if (percent >= 90)
-        {
-            Console.WriteLine("Congratulatons! You passed the class with an A.");
-        }
-        else if (percent >= 80)
-        {
-            Console.WriteLine("Great job! You passed the class with a B.");
-        }
-        else if (percent >= 70)
-        {
-            Console.WriteLine("Good work! You passed the class with a C.");
-        }
-        else if (percent >= 60)
-        {
-            Console.WriteLine("You earned a D. Joining a study group could help your grade.");
-        }
-        else
-        {
-            Console.WriteLine("Unfortunately you failed the class.");
-        }
+        
     }
 
 }
