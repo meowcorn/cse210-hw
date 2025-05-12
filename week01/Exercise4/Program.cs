f0e70666ebc242ee889ce8c08ba46d0b1b6f7dc8
maalcorn <meowcorn@icloud.com>
2025-05-12 00:57:24 -0700

    Sum, Average, Maximum, in group of numbers.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
         Console.WriteLine("Hello World! This is the Exercise 4 Project.");
         List<int> numbers = new List<int>();
         int userNumber = -1;
@@ -19,29 +19,29 @@ class Program
             if (userNumber != 0)
                 numbers.Add(userNumber);
         }
-        
-            int sum = 0;
-            foreach (int number in numbers)
-                sum += number;
-            Console.WriteLine($"The sum is: {sum}");
-  
-            if (numbers.Count > 0)
-        {           
-    
-        float average = ((float)sum) / numbers.Count;
-            Console.WriteLine($"The average is: {average}");
-        
-        int max = numbers[0];
+
+        int sum = 0;
         foreach (int number in numbers)
+            sum += number;
+        Console.WriteLine($"The sum is: {sum}");
+
+        if (numbers.Count > 0)
+        {
+
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            int max = numbers[0];
+            foreach (int number in numbers)
             {
                 if (number > max)
                     max = number;
             }
 
-                Console.WriteLine($"The max is: {max}");
-            }
+            Console.WriteLine($"The max is: {max}");
         }
-    }          
+    }
+}
 
 
 
