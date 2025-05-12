217f35178df5a91b15740d8f40c113c21bd76a26
maalcorn <meowcorn@icloud.com>
2025-05-11 18:28:39 -0700

    Guess the Magic Number game.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = 8;
+        Console.WriteLine("Hello World! This is the Exercise 3 Project.");
+        int guess = 0;
+     
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess a number between 1 and 20.");
+            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your guess is: " + guess);
+            if (guess < magicNumber)
+                Console.WriteLine("Guess again higher.");
+            if (guess > magicNumber)
+                Console.WriteLine("Guess again lower");
+            else if (guess == magicNumber)
+                Console.WriteLine("You guessed the correct number!");
+        }
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 21);
+        int number = 0;
+        while (number != randomNumber)
+        {
+            Console.WriteLine("Guess a number between 1 and 20.");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your guess is: " + number);
+            if (number < randomNumber)
+                Console.WriteLine("Guess again higher.");
+            if (number > randomNumber)
+                Console.WriteLine("Guess again lower");
+            else if (number == randomNumber)
+                Console.WriteLine("You guessed the correct number!");
+        }
+            
     }
 }
+
