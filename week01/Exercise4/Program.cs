d4f45a2cba8fa3fe4ab6b6c2fd1d5e6c408191c5
maalcorn <meowcorn@icloud.com>
2025-05-11 20:17:18 -0700

    uess

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
-
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
-}
+        
+        Console.WriteLine("Hello World! This is the Exercise 4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number. Enter 0 to stop: ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber != 0)
+                numbers.Add(userNumber);
+        }
+        
+            int sum = 0;
+            foreach (int number in numbers)
+                sum += number;
+            Console.WriteLine($"The sum is: {sum}");
+  
+            if (numbers.Count > 0)
+        {           
+    
+        float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+        
+        int max = numbers[0];
+        foreach (int number in numbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+
+                Console.WriteLine($"The max is: {max}");
+            }
+        }
+    }          
+
+
+
+
